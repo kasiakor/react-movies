988840d70795de2250001f2b4c11abd4ed158821
kasia.kordys <kasia.kordys@gmail.com>
2023-09-18 21:59:43 +0300

    fetch movies from remote api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import { useEffect } from "react";
+
 function App() {
+  const API_URL = "https://www.omdbapi.com?apikey=";
+
+  const searchMovies = async (title) => {
+    const res = await fetch(`${API_URL}&s=${title}`);
+    const data = await res.json();
+    console.log("data", data);
+  };
+
+  useEffect(() => {
+    searchMovies("Braveheart");
+  }, []);
+
   return (
     <div className="App">
       <h1>Hello React</h1>
